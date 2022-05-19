63ae4da2e016b036627c0feaa7e9f9cec2d5dbc9
ekamelinda <ekamel127@icloud.com>
2022-05-19 09:23:05 +0700

    ekamelinda

---
+++ app/Controllers/Cobacontroller.php
@@ -0,0 +1,13 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Cobacontroller extends Controller{
+    public function index()
+    {
+        return view('coba2');
+    }
+
+
+}
